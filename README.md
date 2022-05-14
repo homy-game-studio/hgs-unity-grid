11cc9ff476424b835b58079f6dc38b4b3791810e
Matheus Moura <matheus.moura13@gmail.com>
2022-05-14 11:40:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+hgs-unity-grid
---
+++ Runtime/Scripts/SO/GridBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HGS.Grid
+{
+  public class GridBase : ScriptableObject
+  {
+    public Vector3 CoordToPosition()
+    {
+      return Vector3.zero;
+    }
+  }
+}
