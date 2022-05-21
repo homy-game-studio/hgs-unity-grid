28ebb2cd8a9a85b902219e48922816cca7516bbe
Matheus Moura <matheus.moura13@gmail.com>
2022-05-21 16:03:20 -0300

    perf: cria classes auxiliares para  calculo de hexagonos

--- Runtime/Scripts/SO/Grids/GridBase.cs
+++ Runtime/Scripts/SO/Grids/GridBase.cs
@@ -6,9 +6,12 @@ namespace HGS.Grid
 {
   public abstract class GridBase : ScriptableObject
   {
-    [Header("Base config")]
-    public float ceilSize = 1;
+    [Header("Ceil")]
+    public Vector2 ceilSize = Vector2.one;
+
+    [Header("Grid")]
     public Vector3 worldPosition = Vector3.zero;
+    public Vector2Int size = new Vector2Int(5, 5);
 
     public abstract Vector3 CoordToWorldPos(Vector3Int coord);
     public abstract Vector3Int WorldPosToCoord(Vector3 pos);
